ca79b5f1327440c780507f494badc8a0b08878fe
Carnival Build <build@carnivallabs.com>
2013-12-14 12:48:19 +1300

    built Carnival SDK version 1.43

--- Carnival.embeddedframework/Carnival.framework/Versions/A/Headers/CarnivalMessageStream.h
+++ Carnival.embeddedframework/Carnival.framework/Versions/A/Headers/CarnivalMessageStream.h
@@ -9,15 +9,15 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-extern NSString * const CarnivalStreamWillShowStreamNotification;
-extern NSString * const CarnivalStreamDidShowStreamNotification;
-extern NSString * const CarnivalStreamWillDismissStreamNotification;
-extern NSString * const CarnivalStreamDidDismissStreamNotification;
+extern NSString * const CarnivalMessageStreamWillShowStreamNotification;
+extern NSString * const CarnivalMessageStreamDidShowStreamNotification;
+extern NSString * const CarnivalMessageStreamWillDismissStreamNotification;
+extern NSString * const CarnivalMessageStreamDidDismissStreamNotification;
 
-@interface CarnivalStream : NSObject
+@interface CarnivalMessageStream : NSObject
 
 /**
- *  Shows the stream using the specified duration and completion handler
+ *  Shows the message stream using the specified duration and completion handler
  *
  *  @param duration The total duration of the animations, measured in seconds. If you specify a negative value or 0, the changes are made without animating them.
  *  @param completion A block object to be executed when the animation sequence ends. This block has no return value and takes a single Boolean argument that indicates whether or not the animations actually finished before the completion handler was called. If the duration of the animation is 0, this block is performed at the beginning of the next run loop cycle. This parameter may be NULL.
@@ -25,10 +25,10 @@ extern NSString * const CarnivalStreamDidDismissStreamNotification;
  *  Note: On iOS versions below 5, this method does nothing.
  */
 
-+ (void)showStreamWithDuration:(CGFloat)duration completion:(void (^)(BOOL finished))completion;
++ (void)showMessageStreamWithDuration:(CGFloat)duration completion:(void (^)(BOOL finished))completion;
 
 /**
- *  Dismisses the stream using the specified duration and completion handler
+ *  Dismisses the message stream using the specified duration and completion handler
  *
  *  @param duration The total duration of the animations, measured in seconds. If you specify a negative value or 0, the changes are made without animating them.
  *  @param completion A block object to be executed when the animation sequence ends. This block has no return value and takes a single Boolean argument that indicates whether or not the animations actually finished before the completion handler was called. If the duration of the animation is 0, this block is performed at the beginning of the next run loop cycle. This parameter may be NULL.
@@ -36,6 +36,6 @@ extern NSString * const CarnivalStreamDidDismissStreamNotification;
  *  Note: On iOS versions below 5, this method does nothing.
  */
 
-+ (void)dismissStreamWithDuration:(CGFloat)duration completion:(void (^)(BOOL finished))completion;
++ (void)dismissMessageStreamWithDuration:(CGFloat)duration completion:(void (^)(BOOL finished))completion;
 
 @end
